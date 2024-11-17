a91d0c811d7b011cde3775382a7043ccce234cc3
Kosina Zoltán <kosinaz@gmail.com>
2024-11-17 11:38:09 +0100

    basic movement

---
+++ Walls of Quadraman/map.gd
@@ -0,0 +1,58 @@
+extends Node
+
+var tween = null
+var map_position = Vector3(0, 0, 0)
+var up_is_down = false
+var left_is_down = false
+var right_is_down = false
+var down_is_down = false
+var config = ConfigFile.new()
+
+func _ready():
+	# warning-ignore:unused_variable
+	var result = config.load("user://config.cfg")
+	config.set_value("progress", "lesson", 0)
+	config.save("user://config.cfg")
+
+func _process(_delta):
+	if tween != null and tween.is_running():
+		return
+	if Input.is_action_pressed("ui_left") or left_is_down:
+		map_position.x -= 1
+		print("left")
+	elif Input.is_action_pressed("ui_right") or right_is_down:
+		map_position.x += 1
+	elif Input.is_action_pressed("ui_up") or up_is_down:
+		map_position.z -= 1
+	elif Input.is_action_pressed("ui_down") or down_is_down:
+		map_position.z += 1
+	else:
+		return
+	tween = get_tree().create_tween()
+	print(map_position)
+	print($"%Player".translation)
+	tween.tween_property($"%Player", "translation", map_position, 0.25)
+
+func _on_up_button_down():
+	up_is_down = true
+
+func _on_left_button_down():
+	left_is_down = true
+
+func _on_down_button_down():
+	down_is_down = true
+
+func _on_right_button_down():
+	right_is_down = true
+
+func _on_up_button_up():
+	up_is_down = false
+
+func _on_left_button_up():
+	left_is_down = false
+
+func _on_down_button_up():
+	down_is_down = false
+
+func _on_right_button_up():
+	right_is_down = false
