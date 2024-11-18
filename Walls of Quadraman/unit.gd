6f2473d538544fdbfe639a8e9f8f9d93701caa6b
Kosina Zoltán <kosinaz@gmail.com>
2024-11-18 22:36:32 +0100

    complete the stairs logic and map3

--- Walls of Quadraman/unit.gd
+++ Walls of Quadraman/unit.gd
@@ -24,6 +24,15 @@ func move_towards(direction):
 			return false
 		if _degrees[-direction] == siege_tower.rotation_degrees.y:
 			y_translation.y -= 1
+	elif translation.y == 0.5:
+		var stairs = _get_group_member_at("stairs", translation - Vector3(0, 0.5, 0))
+		if stairs:
+			if _degrees[direction] == stairs.rotation_degrees.y: 
+				y_translation.y -= 0.5
+			elif _degrees[-direction] == stairs.rotation_degrees.y:
+				y_translation.y += 0.5
+			else:
+				return false
 	elif translation.y == 1:
 		if (
 			not _is_group_member_at("walls", translation + direction - Vector3(0, 1, 0)) and 
@@ -40,13 +49,13 @@ func move_towards(direction):
 		):
 			return false
 		var stairs = _get_group_member_at("stairs", translation + direction - Vector3(0, 1, 0))
-		if (
-			stairs and
-			_degrees[direction] != stairs.rotation_degrees.y and 
-			_degrees[-direction] != stairs.rotation_degrees.y
-		):
-			return false
-		y_translation.y -= 0.5
+		if stairs:
+			if (
+				_degrees[direction] != stairs.rotation_degrees.y and 
+				_degrees[-direction] != stairs.rotation_degrees.y
+			):
+				return false
+			y_translation.y -= 0.5
 	if _is_group_member_at("doors", translation + direction):
 		if not _is_group_member_at("rams", translation):
 			return false
@@ -57,6 +66,8 @@ func move_towards(direction):
 		if not moved:
 			return false
 	if _is_group_member_at("siege_towers", translation + direction):
+		if _is_group_member_at("rams", translation):
+			return false
 		var siege_tower = _get_group_member_at("siege_towers", translation + direction)
 		var moved = siege_tower.move_towards(direction)
 		if not moved:
@@ -67,6 +78,13 @@ func move_towards(direction):
 			):
 				return false
 			y_translation.y += 1
+	if _is_group_member_at("stairs", translation + direction):
+		var stairs = _get_group_member_at("stairs", translation + direction)
+		if stairs:
+			if _degrees[-direction] == stairs.rotation_degrees.y:
+				y_translation.y += 0.5
+			else:
+				return false
 	if has_node("Spatial/character-male-b/AnimationPlayer"):
 		$"Spatial/character-male-b/AnimationPlayer".play("sprint")
 	rotation_degrees.y = _degrees[direction]
